3bdeeec339feff0cf05d2eeb44a3a2af0398c6db
Thanh10101 <trinhthanh20022@gmail.com>
2022-12-07 08:41:09 +0700

    first commit

---
+++ WebApplication2/Models/Doibong.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication2.Models
+{
+    public class Doibong
+    {
+        public int Id { get; set; }
+        public string? TenQuocGia { get; set; }
+        public int   SoCauThu { get; set; }
+        public int IdCauThu { get; set; }
+
+    }
+}
